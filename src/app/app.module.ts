ae8572c6ba9586fc6f3b88ea528dc0c87a436067
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-03-30 17:46:40 -0300

    Add missing change from previous commits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -320,6 +320,7 @@ interface StoreType  {
     NewOrganizationDialogComponent,
     RegistrationDialogComponent,
     ReportDialogComponent,
+    SearchbarComponent,
     TextInputDialogComponent,
   ]
 })
