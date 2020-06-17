06a606c247241d5d656db65cb0cb66169ff669eb
German Mas <german.e.mas@gmail.com>
2020-06-17 14:59:28 +0000

    Websocket and Visualizer in Admin Component

---
+++ src/app/cloudsim/websocket/topic.ts
@@ -0,0 +1,15 @@
+/**
+ * Type that represents a topic to be subscribed. This allows communication between Components and
+ * the Websocket service of a Simulation.
+ */
+export interface Topic {
+  /**
+   * The name of the topic, used to identify it.
+   */
+  name: string;
+
+  /**
+   * Callback to use for this current topic.
+   */
+  cb(msg: any): any;
+}
