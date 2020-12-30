ccb1487715941389cccf5457d04c785a086f04d4
ChawinTan <chawin15@gmail.com>
2020-12-30 13:15:36 +0800

    make review-model unit test work

--- src/app/model/review/review-model.component.spec.ts
+++ src/app/model/review/review-model.component.spec.ts
@@ -1,6 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatCardModule } from '@angular/material/card';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { PageTitleComponent } from '../../page-title';
+import { AngularEditorModule } from '@kolkov/angular-editor';
 import { ReviewComponent } from './review-model.component';
+import { ModelService } from '../model.service';
+import { AuthService } from '../../auth/auth.service';
+import { JsonClassFactoryService } from '../../factory/json-class-factory.service';
 
 describe('ReviewComponent', () => {
   let component: ReviewComponent;
@@ -8,7 +22,41 @@ describe('ReviewComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ReviewComponent ]
+      imports: [
+        HttpClientTestingModule,
+        MatCardModule,
+        RouterTestingModule,
+        MatToolbarModule,
+        AngularEditorModule,
+        MatIconModule,
+        MatSelectModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        ReactiveFormsModule,
+      ],
+      declarations: [ 
+        ReviewComponent,
+        PageTitleComponent,
+      ],
+      providers: [
+        ModelService,
+        AuthService,
+        JsonClassFactoryService,
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              data: {
+                resolvedData: [],
+              },
+              paramMap: new Map([
+                ['modelname', 'test-model'],
+                ['owner', 'test-owner']
+              ])
+            }
+          }
+        },
+      ]
     })
     .compileComponents();
   });
