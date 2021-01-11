83aa58f37a30bdd24a087c8425f4f516f18083f4
Nathan Koenig <natekoenig@gmail.com>
2021-01-11 16:57:08 +0000

    Expanded 'Information' to include API documentation
    Signed-off-by: Nate Koenig <nate@openrobotics.org>

---
+++ src/app/information/information.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+declare const SwaggerUIBundle: any;
+
+@Component({
+  selector: 'ign-information',
+  templateUrl: 'information.component.html',
+  styleUrls: ['information.component.scss']
+})
+
+/**
+ * Information Component contains documentation about the Ignition App.
+ */
+export class InformationComponent {
+}
