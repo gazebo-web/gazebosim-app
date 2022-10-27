c9826017a75dcc1701cb887c6f189a4115f474b6
German Mas <german@creativa77.com.ar>
2022-10-27 11:46:35 -0300

    Add pagination to GET List of Fuel Resources

--- src/app/fuel-resource/fuel-resource.service.ts
+++ src/app/fuel-resource/fuel-resource.service.ts
@@ -72,20 +72,29 @@ export abstract class FuelResourceService {
   /**
    * Get the first page of all the resources from the Backend.
    *
-   * @param search Optional search string.
-   * @param categories Optional list to filter by categories.
+   * @param params An object containing possible params for the request. They are the following:
+   *               - search: String. The search parameters to be sent as a "q" query parameter.
+   *               - page: Number. The page of resources to get.
+   *               - per_page: Number. The number of resources to get per page.
    * @returns An observable of the paginated resources.
    */
-   public getList(search: string = ''): Observable<FuelPaginatedResource> {
+   public getList(params?: object): Observable<FuelPaginatedResource> {
     const url = this.getListUrl();
-    let params = new HttpParams();
+    let httpParams = new HttpParams();
 
-    // Append the optional search string to the url.
-    if (search !== '') {
-      params = params.append('q', search);
+    if (params) {
+      if (params['search']) {
+        httpParams = httpParams.append('q', params['search']);
+      }
+      if (params['page']) {
+        httpParams = httpParams.append('page', params['page'].toString());
+      }
+      if (params['per_page']) {
+        httpParams = httpParams.append('per_page', params['per_page'].toString());
+      }
     }
 
-    return this.http.get(url, {observe: 'response', params}).pipe(
+    return this.http.get(url, {observe: 'response', params: httpParams}).pipe(
       map((response) => {
         const paginatedResource = new this.paginatedResourceClass();
         paginatedResource.totalCount = +response.headers.get(
